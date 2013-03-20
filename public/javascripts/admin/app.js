df05d593ef38c3b01d0fef00ae6cbe8f40f6a8b6
Sahar Alkolombra <sahar@mediamagic.co.il>
2013-03-20 22:52:32 +0200

    update from sahar

--- public/javascripts/admin/app.js
+++ public/javascripts/admin/app.js
@@ -1,13 +1,42 @@
 'use strict'; 
 
-angular.module('admin', ['ngResource','ngCookies']).
+angular.module('admin', ['ngResource','ngCookies', 'ui']).
 config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 	$routeProvider.
 		when('/main', {templateUrl: '/views/admin/Main.html', controller: MainCtrl, name:'Main'}).
-		when('/nominees', {templateUrl: '/views/admin/Nominees.html', controller: NomineesCtrl, name:'nominees'}).
-		when('/nominee/:id', {templateUrl: '/views/admin/Nominee.html', controller: NomineeCtrl, name:'nominee'}).
+		when('/lists', {templateUrl: '/views/admin/Lists.html', controller: ListsCtrl, name:'lists'}).		
 		when('/settings', {templateUrl: '/views/admin/Settings.html', controller: SettingsCtrl, name:'settings'}).
 		when('/nominee/:id/settings', {templateUrl: '/views/admin/EditNominee.html', controller: EditNomineeCtrl, name:'nominee'}).
 		when('/voters', {templateUrl: '/views/admin/Voters.html', controller: VotersCtrl, name:'Statistics'}).
 		otherwise({redirectTo: '/main'});
-}])
+}]).
+filter('status', function() {
+	return function(status) { 		
+		if(status) 
+			return 'enabled'
+		
+		return 'disabled';
+	}
+}).
+filter('categories', function() {
+	return function(categories) { 		
+		var arr = [];
+
+		for(var cat in categories){			
+			arr.push(cat);
+		}
+		
+		return arr.join(',');
+	}
+}).filter('category', function() {
+	return function(items, category) { 		
+		var arr = [];
+		
+		for(var item in items){						
+			if(items[item].categories[category] != undefined)
+				arr.push(items[item])
+		}
+
+		return arr;
+	}
+})
--- public/javascripts/admin/controllers.js
+++ public/javascripts/admin/controllers.js
@@ -1,5 +1,5 @@
 'user strict';
-angular.module('admin.controllers', ['ngResource', 'ngCookies']);
+angular.module('admin.controllers', ['ngResource', 'ngCookies', 'ui']);
 
 var GlobalCtrl = ['$scope', '$resource', '$location', '$window', '$routeParams','$cookies', function($scope,$resource,$location,$window,$routeParams,$cookies){
 	$scope.window = $window
@@ -17,19 +17,265 @@ var GlobalCtrl = ['$scope', '$resource', '$location', '$window', '$routeParams',
 	$scope.Settings.get({}, function(settings){
 		$scope.settings = settings
 	});
+
+	$scope.filterCategory = 0;
+	$scope.categories = ['family', 'business', 'something', 'someothercat'];	
+
+	$scope.content = [
+						{
+							_id: 5976234598654832,
+							sidebar: {
+										title:'title1',
+										description: 'description1',
+										readMore: {
+													title:'title',
+													url: 'url'
+										}
+							},							
+							categories: {											
+											business:0
+							},					
+							preview: {
+										type:'text/video/flash/iframe/image',
+										link:{
+														type:'none/inner/ext',
+														url:'url to the link',
+														text: 'text on mouse over'
+										},
+										content:'this is preview content',
+										size:'1x1',
+										bgColor:'#fff/green/grey'										
+							},
+							content: {
+										type:'text/video/flash/iframe/image',							
+										content:'this is the content',	
+										bgColor:'#fff/green/grey',										
+							},			
+							dateCreated: new Date(),											
+							status: true
+						},
+						{
+							_id: 5976234598654832,
+							sidebar: {
+										title:'title2',
+										description: 'description1',
+										readMore: {
+													title:'title',
+													url: 'url'
+										}
+							},							
+							categories: {
+											family:0,
+											business:1,
+											something:0
+							},					
+							preview: {
+										type:'text/video/flash/iframe/image',
+										link:{
+														type:'none/inner/ext',
+														url:'url to the link',
+														text: 'text on mouse over'
+										},
+										content:'this is preview content',
+										size:'1x1',
+										bgColor:'#fff/green/grey'										
+							},
+							content: {
+										type:'text/video/flash/iframe/image',							
+										content:'this is the content',	
+										bgColor:'#fff/green/grey',										
+							},			
+							dateCreated: new Date(),											
+							status: false
+						},
+						{
+							_id: 5976234598654832,
+							sidebar: {
+										title:'title3',
+										description: 'description1',
+										readMore: {
+													title:'title',
+													url: 'url'
+										}
+							},							
+							categories: {
+											family:1,
+											business:2
+							},					
+							preview: {
+										type:'text/video/flash/iframe/image',
+										link:{
+														type:'none/inner/ext',
+														url:'url to the link',
+														text: 'text on mouse over'
+										},
+										content:'this is preview content',
+										size:'1x1',
+										bgColor:'#fff/green/grey'										
+							},
+							content: {
+										type:'text/video/flash/iframe/image',							
+										content:'this is the content',	
+										bgColor:'#fff/green/grey',										
+							},			
+							dateCreated: new Date(),											
+							status: true
+						},
+						{
+							_id: 5976234598654832,
+							sidebar: {
+										title:'title4',
+										description: 'description1',
+										readMore: {
+													title:'title',
+													url: 'url'
+										}
+							},							
+							categories: {
+											family:2,
+											business:3
+
+							},					
+							preview: {
+										type:'text/video/flash/iframe/image',
+										link:{
+														type:'none/inner/ext',
+														url:'url to the link',
+														text: 'text on mouse over'
+										},
+										content:'this is preview content',
+										size:'1x1',
+										bgColor:'#fff/green/grey'										
+							},
+							content: {
+										type:'text/video/flash/iframe/image',							
+										content:'this is the content',	
+										bgColor:'#fff/green/grey',										
+							},			
+							dateCreated: new Date(),											
+							status: true
+						},
+						{
+							_id: 5976234598654832,
+							sidebar: {
+										title:'title5',
+										description: 'description1',
+										readMore: {
+													title:'title',
+													url: 'url'
+										}
+							},							
+							categories: {
+											family:3,
+											something:1							
+							},					
+							preview: {
+										type:'text/video/flash/iframe/image',
+										link:{
+														type:'none/inner/ext',
+														url:'url to the link',
+														text: 'text on mouse over'
+										},
+										content:'this is preview content',
+										size:'1x1',
+										bgColor:'#fff/green/grey'										
+							},
+							content: {
+										type:'text/video/flash/iframe/image',							
+										content:'this is the content',	
+										bgColor:'#fff/green/grey',										
+							},			
+							dateCreated: new Date(),											
+							status: true
+						}					
+	]
+
+	//$scope.tempContent = angular.copy($scope.content);
+
+	$scope.category = {};
+	for(var cat = 0; cat < $scope.categories.length; cat++) {
+		$scope.category[$scope.categories[cat]] = angular.copy($scope.content);
+
+		var arr = $scope.category[$scope.categories[cat]];
+		var newArr = [];
+		for(var i = 0; i < arr.length; i++) {			
+			if(arr[i].categories[$scope.categories[cat]] != undefined)
+				newArr.push(arr[i]);
+		}	
+
+		$scope.category[$scope.categories[cat]] = newArr;
+	}
 }];
 
 var MainCtrl = ['$scope', function($scope){
-	$scope.Users.query({}, function(response){
-		$scope.users = response;
-	});
-	$scope.Stats.query({type: 'visit'}, function(resp){
-		$scope.refs = resp;
-	});
-	$scope.Stats.query({type: 'share'}, function(resp){
-		$scope.shares = resp;
-	});
+	// $scope.Users.query({}, function(response){
+	// 	$scope.users = response;
+	// });
+	// $scope.Stats.query({type: 'visit'}, function(resp){
+	// 	$scope.refs = resp;
+	// });
+	// $scope.Stats.query({type: 'share'}, function(resp){
+	// 	$scope.shares = resp;
+	// });
+
+	$scope.toggleStatus = function(id) {
+		console.log(id);
+	}
+
+	$scope.itemPreview = function(id) {
+
+	}
+
+	$scope.itemEdit = function(id) {
+
+	}
 }];
+
+var ListsCtrl = ['$scope', function($scope){
+
+	$scope.showListsSaveButton = true;
+	$scope.sortableOptions = {
+								update: function(event, ui) {									
+									//var fromIndex = ui.item.sortable.index;
+									//var toIndex = $(ui.item).index();
+									
+									if($scope.filterCategory != 0) {
+										// var tempIndexFrom = $scope.tempContent[fromIndex].categories[$scope.filterCategory];
+										// var tempIndexTo = $scope.tempContent[toIndex].categories[$scope.filterCategory];
+										// $scope.tempContent[fromIndex].categories[$scope.filterCategory] = tempIndexTo;
+										// $scope.tempContent[toIndex].categories[$scope.filterCategory] = tempIndexFrom;
+
+										//$scope.category[$scope.filterCategory][fromIndex].categories[$scope.filterCategory] = toIndex;
+										//$scope.category[$scope.filterCategory][toIndex].categories[$scope.filterCategory] = fromIndex;
+
+										// setTimeout(function() {
+										// 	for(var i = 0; i < $scope.category[$scope.filterCategory].length; i++) {
+										// 		var item = $scope.category[$scope.filterCategory][i];
+										// 		item.categories[$scope.filterCategory] = i;
+										// 	}											
+										// }, 2000);
+									}
+								}
+	};
+
+	$scope.orderCategories = function(item) {		
+		//return item.categories[$scope.filterCategory];
+		return $scope.category[$scope.filterCategory];
+	}
+	
+	$scope.previewNewList = function() {
+		$scope.showListsSaveButton = false;
+	}
+
+}];
+
+
+
+
+
+
+
+
 
 var NomineesCtrl = ['$scope', function($scope){
 	$scope.Users.query({}, function(response){
