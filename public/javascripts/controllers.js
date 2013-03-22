2f8ad6373d5bf97547cb2f605f4472edab485ba9
Benny Cohen <benny@mediamagic.co.il>
2013-03-22 12:48:04 +0200

    basic content logic, filtering, sorting, requesting

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('app.controllers', ['ngResource','ngCookies']);
+angular.module('rapid.controllers', ['ngResource','ngCookies']);
 
 var GlobalCtrl = ['$scope', '$resource', '$location', '$window', '$routeParams', '$cookies', '$route', function($scope,$resource,$location,$window,$routeParams,$cookies,$route){
 	$scope.Settings = $resource('/resources/settings')
@@ -15,11 +15,25 @@ var GlobalCtrl = ['$scope', '$resource', '$location', '$window', '$routeParams',
 	$scope.Settings.get({}, function(settings)	{ 
 		$scope.settings = settings
 		, $scope.Stats = $scope.resource('/resources/stats/:type', {_csrf: $cookies['csrf.token']})
-		, $scope.Login = $scope.resource('/api/login', {_csrf: $cookies['csrf.token']}); 
+		, $scope.Login = $scope.resource('/api/login', {_csrf: $cookies['csrf.token']})
+		, $scope.Articles = $scope.resource('/resources/articles/:id')
 	});
+	$scope.filters = { category: 'all' }
 }];
 
 var MainCtrl = ['$scope', function($scope){
+	$scope.getCategory = function(item){
+		if ($scope.filters.category === '')
+			return item;
+		if( item.categories[$scope.filters.category] != undefined)
+			return item;
+	}
+	$scope.orderCategory = function(item){
+		return parseInt(item.categories[$scope.filters.category]);
+	}
+	$scope.Articles.query({}, function(resp){
+		$scope.articles = resp;
+	});
 }];
 
 var LoginCtrl = ['$scope', '$window' , function($scope, $window){
@@ -32,6 +46,6 @@ var LoginCtrl = ['$scope', '$window' , function($scope, $window){
 								if (resp.error === undefined){
 									(prevUrl === undefined) ? $scope.location.path('/admin') : $window.location.href = prevUrl;
 								} 
-		});
+							});
 	}
 }];
