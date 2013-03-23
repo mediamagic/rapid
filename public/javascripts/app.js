c1209f98db576c6a9de1611ed7f2fa54bf8f1231
Benny Cohen <benny@mediamagic.co.il>
2013-03-23 02:11:23 +0200

    isotope directive added

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,3 +7,22 @@ config(['$routeProvider', '$locationProvider', function($routeProvider, $locatio
 		when('/login', {templateUrl: '/views/Login.html', controller: LoginCtrl, name: 'Login'}).
 		otherwise({redirectTo: '/main'});
 }])
+.directive('uiIsotope', function factory(injectables) {
+  var directiveDefinitionObject = {
+    priority: 0,
+    template: '<div></div>',
+    templateUrl: 'directive.html',
+    replace: false,
+    transclude: false,
+    restrict: 'A',
+    scope: false,
+    compile: function compile(tElement, tAttrs, transclude) {
+      return {
+        pre: function preLink(scope, iElement, iAttrs, controller) { ... },
+        post: function postLink(scope, iElement, iAttrs, controller) { ... }
+      }
+    },
+    link: function postLink(scope, iElement, iAttrs) { ... }
+  };
+  return directiveDefinitionObject;
+});
