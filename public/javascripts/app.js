3c5cbcee8508a024b5b0f29aa68200414245781c
Benny Cohen <benny@mediamagic.co.il>
2013-03-24 10:05:56 +0200

    isotope included, directive built, filtering and ordering

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,28 +1,57 @@
 'use strict'; 
 
-angular.module('rapid', ['ngResource', 'ngCookies']).
+angular.module('rapid', ['ngResource', 'ngCookies', 'ui']).
 config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 	$routeProvider.
 		when('/main', {templateUrl: '/views/Main.html', controller: MainCtrl, name:'Main'}).
 		when('/login', {templateUrl: '/views/Login.html', controller: LoginCtrl, name: 'Login'}).
 		otherwise({redirectTo: '/main'});
 }])
-.directive('uiIsotope', function factory(injectables) {
-  var directiveDefinitionObject = {
+.directive('uiIsotope', function() {
+ return {
     priority: 0,
-    template: '<div></div>',
-    templateUrl: 'directive.html',
-    replace: false,
-    transclude: false,
     restrict: 'A',
-    scope: false,
-    compile: function compile(tElement, tAttrs, transclude) {
-      return {
-        pre: function preLink(scope, iElement, iAttrs, controller) { ... },
-        post: function postLink(scope, iElement, iAttrs, controller) { ... }
-      }
-    },
-    link: function postLink(scope, iElement, iAttrs) { ... }
-  };
-  return directiveDefinitionObject;
+    link: function(scope, elm, attr) {
+    	var opts 	= JSON.parse(attr.options)
+    	opts.getSortData = {}
+    	for (var cat in scope.settings.categories) {
+    		var c 	= scope.settings.categories[cat]
+    		opts.getSortData[c] = createOrderFn(c);
+    	}
+    	setTimeout(function(){
+    		$(elm).isotope(opts).isotope('reloadItems').isotope({sortBy:'all'});
+    	},100)
+    	scope.$watch('articles',function(n,o){
+    		if (n!=o)
+				$(elm).isotope('reloadItems');
+    	})
+    	scope.$watch('filters.category', function(n,o){
+  			$(elm).isotope({sortBy:n});
+    	})
+    }
+  }
+})
+.directive('isoItem', function() {
+ return {
+    priority: 0,
+    restrict: 'A',
+    link: function(scope, elm, attr) {
+    	$(elm).bind('click', function(){
+    		$(this).removeClass('preview').addClass('open');
+    		$(elm).parent().isotope('reLayout', function(){
+    			
+    		});
+    	})
+    }
+  }
 });
+
+//some closure magic
+function createOrderFn(cat){
+	var c = cat;
+	return function(elm){
+		var rel = $(elm).attr('rel')
+			, tmp =  JSON.parse(rel)
+		return parseInt(tmp[c]);
+	}
+}
--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('rapid.controllers', ['ngResource','ngCookies']);
+angular.module('rapid.controllers', ['ngResource','ngCookies', 'ui']);
 
 var GlobalCtrl = ['$scope', '$resource', '$location', '$window', '$routeParams', '$cookies', '$route', function($scope,$resource,$location,$window,$routeParams,$cookies,$route){
 	$scope.Settings = $resource('/resources/settings')
@@ -16,23 +16,41 @@ var GlobalCtrl = ['$scope', '$resource', '$location', '$window', '$routeParams',
 		$scope.settings = settings
 		, $scope.Stats = $scope.resource('/resources/stats/:type', {_csrf: $cookies['csrf.token']})
 		, $scope.Login = $scope.resource('/api/login', {_csrf: $cookies['csrf.token']})
-		, $scope.Articles = $scope.resource('/resources/articles/:id')
 	});
 	$scope.filters = { category: 'all' }
 }];
 
 var MainCtrl = ['$scope', function($scope){
-	$scope.getCategory = function(item){
-		if ($scope.filters.category === '')
-			return item;
-		if( item.categories[$scope.filters.category] != undefined)
-			return item;
+	$scope.Articles 		= $scope.resource('/resources/articles/:id')
+	$scope.contentLoaded 	= false;
+
+	$scope.getCategory = function(index){
+		var item 	= $scope.articles[index]
+			, arr 	= []
+			, cat = $scope.filters.category
+		arr.push('size'+item.preview.size);
+		if (item.categories[cat] > $scope.articles.length)
+			arr.push('disabled');
+		(item.preview.link.type == 'none') ? 
+			arr.push('unlinked') : arr.push('linked')
+		return arr;
 	}
-	$scope.orderCategory = function(item){
-		return parseInt(item.categories[$scope.filters.category]);
-	}
+
 	$scope.Articles.query({}, function(resp){
+		var cats = $scope.settings.categories
+			, l = resp.length
+		for (var cat in resp){
+			var itm = resp[cat];
+			for (var i = 0;i<cats.length;i++){
+				var c = cats[i];
+				if (itm.categories[c] == undefined) {
+					itm.categories[c] = l;
+					l++
+				}
+			}
+		}
 		$scope.articles = resp;
+		$scope.contentLoaded = true;
 	});
 }];
 
