95b4a0d9ab6648dd4554fbe58f3ca075c97164a2
Benny Cohen <benny@mediamagic.co.il>
2013-03-27 01:18:45 +0200

    -removal of file returns a json object with error:0 instead of a string

--- controllers/swfs.js
+++ controllers/swfs.js
@@ -58,7 +58,7 @@ module.exports = function(db){
 				fs.unlink(global.root + "public/images/swfs/"+hashName, function (err) {
 				  if (err)
 				  	return res.send(handle(err,null));
-				  return res.send(handle(null,'ok'));
+				  return res.send(handle(null,'{error:0}'));
 				});
 			})
 		}
